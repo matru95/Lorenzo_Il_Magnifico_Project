75f45088d35d4bca8fa9f32fddbfbc8a2c52b985
matru95 <matteo.rubiu@mail.polimi.it>
2017-05-18 10:00:56 +0200

    Added initial SpaceWrapper classes

---
+++ src/main/java/it/polimi/ingsw/GC_31/MartWrapper.java
@@ -0,0 +1,7 @@
+package it.polimi.ingsw.GC_31;
+
+/**
+ * Created by matte on 18/05/2017.
+ */
+public class MartWrapper {
+}
---
+++ src/main/java/it/polimi/ingsw/GC_31/TowerSpaceWrapper.java
@@ -0,0 +1,4 @@
+package it.polimi.ingsw.GC_31;
+
+public class TowerSpaceWrapper {
+}
