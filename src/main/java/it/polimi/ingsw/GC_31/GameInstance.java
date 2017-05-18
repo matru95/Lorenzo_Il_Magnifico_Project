e1f9800f92de1551f6c308b53cb1907be44cc714
Endi Sukaj <endisukaj@gmail.com>
2017-05-18 10:44:34 +0200

    GameInstance player creation.
    GameInstance now should:
    - Ask for number of players and check if 2 <= number <= 4.
    - Ask for player name.
    - Ask for player color and only choose if color is available and not
    already chosen.

--- src/main/java/it/polimi/ingsw/GC_31/GameInstance.java
+++ src/main/java/it/polimi/ingsw/GC_31/GameInstance.java
@@ -1,11 +1,18 @@
 package it.polimi.ingsw.GC_31;
 
-import java.io.Console;
+import java.util.List;
+import java.util.Arrays;
+import java.util.InputMismatchException;
+import java.util.LinkedList;
 import java.util.Scanner;
 
 public class GameInstance {
+	private Scanner in = new Scanner(System.in);
+	private List<PlayerColor> availableColors = new LinkedList<PlayerColor>(Arrays.asList(PlayerColor.values()));
+
 	private GameBoard gameBoard;
 	private Player[] players;
+
 	private int numOfPlayers;
 	private int age;
 	private int turn;
@@ -17,39 +24,72 @@ public class GameInstance {
 		for(int i=0; i<numOfPlayers; i++) {
 			Player player = askPlayerNameAndColor(i);
 			players[i] = player;
-			
 		}
 		
+//		Initialize first turn and first age.
 		age = 1;
 		turn = 1;
+		in.close();
+		
 	}
 	
 	private int askNumOfPlayers() {
 		System.out.println("Quanti giocatori ci sono?");
-		Scanner in = new Scanner(System.in);
-		int numOfPlayers = in.nextInt();
-		return numOfPlayers;
 		
+		try {
+			int numOfPlayers = in.nextInt();
+			System.out.println(numOfPlayers);
+			
+			if(numOfPlayers < 2) {
+				System.out.println("Il numero minimo dei giocatori e 2");
+				return askNumOfPlayers();
+			} else if(numOfPlayers > 4) {
+				System.out.println("Il numero massimo dei giocatori e 4");
+				return askNumOfPlayers();
+			} else {
+				return numOfPlayers;
+			}
+
+		} catch(InputMismatchException e) {
+//			Maybe find a way to handle this exception in a better way?
+			System.out.println("Il numero dei giocatori deve essere un numero.");
+			System.exit(1);
+			return 0;
+		} 
+
 	}
 	
 	private Player askPlayerNameAndColor(int id) {
-		Scanner in = new Scanner(System.in);
-		String color;
+		int currentPlayer = id + 1;
+		String colorName;
+		PlayerColor chosenColor;
 		
-//		TODO Add current player number
-		System.out.println("Come si chiama il giocatore?");
-		String name = in.next();
+		System.out.println("Come si chiama il giocatore numero " + currentPlayer + "?");
+		String playerName = in.next();
 		
 		while(true) {
-//			TODO 
 			System.out.println("Quale colore ha?");
-			color = in.next();
-			if(PlayerColor.contains(color)) {
-				break;
+			
+			for(PlayerColor c : availableColors) {
+				System.out.println(c.name());
+			}
+
+			colorName = in.next();
+			
+			if(PlayerColor.contains(colorName)) {
+				chosenColor = PlayerColor.valueOf(colorName.toUpperCase());
+				
+				if(availableColors.contains(chosenColor)) {
+					availableColors.remove(chosenColor);
+					break;
+				} else {
+					System.out.println("Quel colore e gia stato scelto!");
+				}
+
 			}
 		}
 		
-		Player player = new Player(id, name, PlayerColor.valueOf(color));
+		Player player = new Player(id, playerName, chosenColor);
 		return player;
 		
 		
--- src/main/java/it/polimi/ingsw/GC_31/Player.java
+++ src/main/java/it/polimi/ingsw/GC_31/Player.java
@@ -4,8 +4,9 @@ enum PlayerColor {
 	RED, BLUE, GREEN, YELLOW;
 	
 	public static Boolean contains(String value) {
+		String upperCaseValue = value.toUpperCase();
 		for(PlayerColor color : PlayerColor.values()) {
-			if(color.name().equals(value)) {
+			if(color.name().equals(upperCaseValue)) {
 				return true;
 			}
 		}
