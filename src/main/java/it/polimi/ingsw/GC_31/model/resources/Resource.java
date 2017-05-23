8f493dc11605aaad75242f12702e3a9ac9e9afb2
Matteo Rubiu <matteo.rubiu@mail.polimi.it>
2017-05-23 15:49:26 +0200

    Added error handling to resource methods

--- src/main/java/it/polimi/ingsw/GC_31/model/resources/Resource.java
+++ src/main/java/it/polimi/ingsw/GC_31/model/resources/Resource.java
@@ -5,14 +5,26 @@ public abstract class Resource {
     public abstract int getNumOf();
     public abstract void setNumOf(int numOf);
 
-    public void addNumOf(int value) {
-
-        this.setNumOf(this.getNumOf() + value);
+    public void addNumOf(int value) throws IllegalArgumentException {
+        try {
+            if (value > 0) {
+                this.setNumOf(this.getNumOf() + value);
+            }
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+            System.err.println("Value must be greater then zero");
+        }
     }
 
     public void subNumOf(int value) {
-
-        this.setNumOf(this.getNumOf() - value);
+        try {
+            if (value > 0 && value < this.getNumOf()) {
+                this.setNumOf(this.getNumOf() - value);
+            }
+        } catch (IllegalArgumentException e) {
+            System.err.println("Value must be greater then zero and smaller then actual value");
+            e.printStackTrace();
+        }
     }
 }
 
