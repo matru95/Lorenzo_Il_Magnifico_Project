4b47dc8c0925e79229d3a851feb774af4c10376b
leonardo <l.panerai1995@gmail.com>
2017-05-27 15:39:43 +0200

    Fixed card json ecc..

--- src/test/java/it/polimi/ingsw/gc31/NoResourceMatchTest.java
+++ src/test/java/it/polimi/ingsw/gc31/NoResourceMatchTest.java
@@ -1,5 +1,6 @@
 package it.polimi.ingsw.gc31;
 
+import it.polimi.ingsw.gc31.model.resources.NoResourceMatch;
 import it.polimi.ingsw.gc31.model.resources.Resource;
 import it.polimi.ingsw.gc31.model.resources.ResourceFactory;
 import org.junit.Test;
@@ -8,16 +9,15 @@ import static org.junit.Assert.assertTrue;
 
 public class NoResourceMatchTest extends Throwable {
     @Test
-    public void testResourceFactoryGetResourceThrowsNoResourceMatch() {
+    public void test() {
         boolean thrown = false;
 
         try {
             Resource gold= null;
-            gold=ResourceFactory.getResource("Golds",3);
-        } catch (it.polimi.ingsw.gc31.model.resources.NoResourceMatch e) {
+            gold= ResourceFactory.getResource("Gold",3);
+        } catch (NoResourceMatch e) {
             thrown = true;
         }
-
         assertTrue(thrown);
     }
 }
