f72b9c962e05de5162edbd1467542972c72f5ecf
Endi Sukaj <endisukaj@gmail.com>
2017-06-06 16:53:12 +0200

    Preparing to merge with master

--- src/main/java/it/polimi/ingsw/gc31/model/states/GamePrepState.java
+++ src/main/java/it/polimi/ingsw/gc31/model/states/GamePrepState.java
@@ -14,16 +14,8 @@ public class GamePrepState implements State{
         ArrayList<Player> players = context.getPlayers();
         numOfPlayers = context.getNumOfPlayers();
 
-        blockSpacesIfNeeded(context);
     }
 
-    private void blockSpacesIfNeeded(GameInstance context) {
-        if(this.numOfPlayers == 2) {
-            context.getGameBoard().blockSpacesTwoPlayers();
-        } else if(this.numOfPlayers == 3) {
-            context.getGameBoard().blockSpacesThreePlayers();
-        }
-    }
 
     private void distributeResources(Player[] players) {
         for(Player player: players) {
