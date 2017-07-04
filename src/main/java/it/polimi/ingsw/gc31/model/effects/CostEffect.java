e21f1bf96128c1b212e7c5f7ed5aff990f7d9e07
Leonardo Panerai <l.panerai1995@gmail.com>
2017-07-05 00:06:44 +0200

    fixed CostEffect

--- src/main/java/it/polimi/ingsw/gc31/model/effects/CostEffect.java
+++ src/main/java/it/polimi/ingsw/gc31/model/effects/CostEffect.java
@@ -1,62 +1,35 @@
 package it.polimi.ingsw.gc31.model.effects;
 
+import it.polimi.ingsw.gc31.enumerations.CardColor;
 import it.polimi.ingsw.gc31.messages.ServerMessage;
+import it.polimi.ingsw.gc31.messages.ServerMessageEnum;
 import it.polimi.ingsw.gc31.model.Player;
 import it.polimi.ingsw.gc31.exceptions.NoResourceMatch;
-import it.polimi.ingsw.gc31.model.resources.Resource;
-import it.polimi.ingsw.gc31.enumerations.ResourceName;
+import it.polimi.ingsw.gc31.model.cards.Card;
 import jdk.nashorn.internal.ir.ObjectNode;
 
+
 import java.util.HashMap;
-import java.util.Scanner;
+import java.util.Map;
 
 //TODO implementare il controllo nella board o nella classe player o...?
 public class CostEffect extends Effect {
-    private HashMap res1;
-    private HashMap subRes1;
-    private HashMap res2;
-    private HashMap subRes2;
-    private String playerchoice;
-    CostEffect(HashMap res1, HashMap subRes1, HashMap res2, HashMap subRes2,String playerchoice){
-        this.res1=res1;
-        this.res2=res2;
-        this.subRes1=subRes1;
-        this.subRes2=subRes2;
-        this.playerchoice=playerchoice;
+    private Card purpleCard;
+
+    CostEffect(Card purpleCard){
+        this.purpleCard=purpleCard;
     }
 
     @Override
     public ServerMessage exec(Player player) throws NoResourceMatch {
-//--------------------CREAZIONE OGGETTI NECESSARI PER EXEC
-
-        HashMap<ResourceName, Resource> startingmap1 = this.res1;
-        HashMap<ResourceName, Resource> startingmap2 = this.res2;
-        HashMap<ResourceName, Resource> subingmap1 = this.subRes1;
-        HashMap<ResourceName, Resource> subingmap2 = this.subRes2;
-//-------------------CONTROLLO DEL VALORE DELLA SCELTA
-        do {
-            System.out.println("Digitare 1 per pagare nel primo modo, 2 per pagare nel secondo");
-            Scanner input = new Scanner(System.in);
-            playerchoice = input.nextLine();
-        }while("1".equals(playerchoice) || "2".equals(playerchoice));
-
-//-------------------------ESEGUO IL METODO SCELTO DAL PLAYER
-
-        //PRIMO METODO DI PAGAMENTO
-        if(startingmap2==null || "1".equals(playerchoice)) {
-            for (ResourceName key : startingmap1.keySet()) {
-                startingmap1.get(key).subNumOf(subingmap1.get(key).getNumOf());
-            }
-            player.setRes(startingmap1);
+        if(this.purpleCard.getCost().size()==2 && this.purpleCard.getCardColor()== CardColor.PURPLE) {
+            Map<String, String> payload = new HashMap<>();
+            payload.put("cardID",String.valueOf(purpleCard.getCardID()));
+            ServerMessage request = new ServerMessage(ServerMessageEnum.COSTREQUEST,payload);
+            return request;
+        } else {
+            return null;
         }
-        //SECONDO METODO DI PAGAMENTO
-        if(startingmap2!=null && "2".equals(playerchoice)) {
-            for (ResourceName key : startingmap2.keySet()) {
-                startingmap2.get(key).subNumOf(subingmap2.get(key).getNumOf());
-            }
-            player.setRes(startingmap2);
-        }
-        return null;
     }
 
     @Override
