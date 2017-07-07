2e1a490cac5fbafa860d4ba3ade478174c783828
Leonardo Panerai <l.panerai1995@gmail.com>
2017-07-08 01:36:48 +0200

    Implemented some of FaitTile Malus

--- src/main/java/it/polimi/ingsw/gc31/model/effects/boardeffects/HarvestEffect.java
+++ src/main/java/it/polimi/ingsw/gc31/model/effects/boardeffects/HarvestEffect.java
@@ -6,6 +6,9 @@ import it.polimi.ingsw.gc31.model.Player;
 import it.polimi.ingsw.gc31.model.cards.Card;
 import it.polimi.ingsw.gc31.model.effects.AddResEffect;
 import it.polimi.ingsw.gc31.model.effects.Effect;
+import it.polimi.ingsw.gc31.model.effects.permanent.HarvestMalus;
+import it.polimi.ingsw.gc31.model.effects.permanent.Malus;
+import it.polimi.ingsw.gc31.model.effects.permanent.MalusEnum;
 import it.polimi.ingsw.gc31.model.resources.Resource;
 
 import java.util.ArrayList;
@@ -23,10 +26,16 @@ public class HarvestEffect implements BoardEffect{
         Effect harvestTile = new AddResEffect(harvestTileResources);
 
         harvestTile.exec(player);
-
-
+        int diceValue=0;
+        List<Malus> maluses=player.getMaluses();
+        for(Malus malus:maluses){
+            if(malus.getMalusType()== MalusEnum.HARVESTMALUS){
+                diceValue = ((HarvestMalus) malus).getHarvestFewer();
+            }
+        }
+        int finalValue= value-diceValue;
         for (Card singleCard : greenCards) {
-            if (singleCard.getActivationValue() <= value) {
+            if (singleCard.getActivationValue() <= finalValue) {
                 List<ServerMessage> cardMessages = singleCard.execNormalEffect(player);
 
                 for(ServerMessage cardMessage: cardMessages) {
